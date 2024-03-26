c496804f2400449b843c8e02d94f02068cbbbacf
classe3I1 <classe3I1@DX24.local>
2024-03-26 09:05:26 +0100

    first commit

---
+++ cassataCarRacing.java
@@ -0,0 +1,19 @@
+package it.edu.iisgubbio.gioco;
+
+import javafx.application.Application;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+
+public class cassataCarRacing extends Application{
+	ImageView macchina = new ImageView("it/edu/iisgubbio/gioco/car.png");
+	ImageView strada = new ImageView("it/edu/iisgubbio/gioco/strada.png");
+	
+	public void start(Stage finestra) {
+		
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+	
+}
