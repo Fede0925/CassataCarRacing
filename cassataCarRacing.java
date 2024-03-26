93ed6c542af57dc453ba806f37428211d166f0cd
classe3I1 <classe3I1@DX24.local>
2024-03-26 09:51:52 +0100

    add sfondo

--- cassataCarRacing.java
+++ cassataCarRacing.java
@@ -6,28 +6,35 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 public class cassataCarRacing extends Application{
 	ImageView macchina = new ImageView("it/edu/iisgubbio/gioco/car.png");
 	ImageView strada = new ImageView("it/edu/iisgubbio/gioco/strada.png");
-	ImageView sfondo = new ImageView("it/edu/iisgubbio/gioco/sfondo.png");
+	
+	Image i = new Image("it/edu/iisgubbio/gioco/sfondo.png");
+	
+	ImageView sfondo = new ImageView(i);
 	
 	Button apri = new Button("GIOCA");
 	
 	public void start(Stage finestra) {
 		GridPane griglia = new GridPane();
 		Scene scena = new Scene(griglia, 500, 500);
+		scena.getStylesheets().add("it/edu/iisgubbio/gioco/style.css");
 		
+		sfondo.setFitWidth(200);
+		sfondo.setFitHeight(100);
+		
+		griglia.setId("pagina1");
 		griglia.setAlignment(Pos.CENTER);
 		griglia.setPadding(new Insets(10));
 		griglia.setHgap(10);
 		griglia.setVgap(10);
 		griglia.add(apri, 0, 0);
-		griglia.add(sfondo, 0, 0);
 		apri.setOnAction(e->apri());
 		
 		finestra.setScene(scena);
