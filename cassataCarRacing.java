42e8ecd6db323dd6f387fdf1fa6b18819da72cfe
Fede0925 <150134483+Fede0925@users.noreply.github.com>
2024-04-18 17:26:01 +0200

    GIOCO FINITO

--- cassataCarRacing.java
+++ cassataCarRacing.java
@@ -22,6 +22,7 @@ import javafx.stage.WindowEvent;
 import javafx.util.Duration;
 
 public class cassataCarRacing extends Application{
+//	immagini e git della finestra seleziona difficoltà
 	Image gatto = new Image("it/edu/iisgubbio/gioco/giphy.gif");
 	ImageView gattoW = new ImageView(gatto);
 	
@@ -39,15 +40,12 @@ public class cassataCarRacing extends Application{
 	
 	Image shrek = new Image("it/edu/iisgubbio/gioco/shrek.jpg");
 	ImageView shrekW = new ImageView(shrek);
-	
+//	immagini della finestra di gioco e selezione del veicolo
 	Image bugs = new Image("it/edu/iisgubbio/gioco/bugs.gif");
 	ImageView bugsW = new ImageView(bugs);
 	
 	Image macchina = new Image("it/edu/iisgubbio/gioco/car.png");
 	ImageView macchinaW = new ImageView(macchina);
-	
-	Image titolo = new Image("it/edu/iisgubbio/gioco/titolo.png");
-	ImageView titoloW = new ImageView(titolo);
 	
 	Image mBlu = new Image("it/edu/iisgubbio/gioco/mBlu.png");
 	ImageView mBluW = new ImageView(mBlu);
@@ -78,12 +76,15 @@ public class cassataCarRacing extends Application{
 	
 	Image game = new Image("it/edu/iisgubbio/gioco/gameOver.png");
 	ImageView gameW = new ImageView(game);
+//	immagini della schermata principale
+	Image titolo = new Image("it/edu/iisgubbio/gioco/titolo.png");
+	ImageView titoloW = new ImageView(titolo);
 	
-	Boolean vivo = true;
 	int posX[] = new int[6];
 	int velS = 11;
 	int velocita = 6;
 	int best = 0;
+	Boolean vivo = true;
 	
 	Button apri = new Button("GIOCA");
 	Button giocatore = new Button("Seleziona veicolo");
@@ -100,7 +101,7 @@ public class cassataCarRacing extends Application{
 		griglia.getChildren().add(apri);
 		griglia.getChildren().add(giocatore);
 		griglia.getChildren().add(difficolta);
-		
+//		grandezza delle macchine
 		mBluW.setFitWidth(35);
 		mBluW.setFitHeight(70);
 		
@@ -118,7 +119,7 @@ public class cassataCarRacing extends Application{
 		
 		moto2W.setFitWidth(35);
 		moto2W.setFitHeight(70);
-		
+//		posizione oggetti della schermata principale
 		titoloW.setX(200);
 		titoloW.setY(25);
 		apri.setLayoutX(350);
@@ -127,15 +128,16 @@ public class cassataCarRacing extends Application{
 		giocatore.setLayoutY(115);
 		difficolta.setLayoutX(500);
 		difficolta.setLayoutY(115);
-		
+//		azioni dei pilsanti
 		apri.setOnAction(e->apri());
 		difficolta.setOnAction(e->difficolta());
 		giocatore.setOnAction(e->veicolo());
-		
+//		id css dei pulstanti e del titolo
 		apri.setId("gioca");
 		titoloW.setId("titolo");
 		giocatore.setId("player");
 		difficolta.setId("difficolta");
+//		grandezza del titolo
 		titoloW.setFitWidth(500);
 		titoloW.setFitHeight(40);
 		
@@ -164,42 +166,54 @@ public class cassataCarRacing extends Application{
 	public class FinestraGioco extends Stage{
 		Timeline tempo = new Timeline(new KeyFrame(Duration.seconds(0.02), x->aggiorna()));
 		Timeline tempo2 = new Timeline(new KeyFrame(Duration.seconds(0.1), x->aggiorna2()));
-		int y = -278;
+//		variabili macchina giocatore
 		int rotazione = 0;
 		double x = 10;
-		int punteggio = 0;
-		Label punti = new Label("punti: " + punteggio);
-		Label lBest = new Label("Best Score: " + best);
 		Boolean veicolo = false;
+//		vettori macchine del traffico
 		ImageView carW1[] = new ImageView[3];
 		ImageView carW2[] = new ImageView[3];
 		ImageView carW3[] = new ImageView[3];
 		ImageView carW4[] = new ImageView[3];
 		ImageView carW5[] = new ImageView[3];
 		ImageView carW6[] = new ImageView[3];
-		Label riavvia = new Label("-PREMI SPAZIO-");
+//		y macchine del traffico
+		int y1=0;
+		int y2=0;
+		int y3=0;
+		int y4=0;
+		int y5=0;
+		int y6=0;
+//		y strada
+		int y = -278;
+//		variabili del punteggio
+		int punteggio = 0;
+		Label punti = new Label("punti: " + punteggio);
+		Label lBest = new Label("Best Score: " + best);
 		
-		int y1=-400;
-		int y2=-400;
-		int y3=-400;
-		int y4=-400;
-		int y5=-400;
-		int y6=-400;
+		Label riavvia = new Label("-PREMI SPAZIO-");
 		
 		  public FinestraGioco(){
 			Pane griglia2 = new Pane();
 			Scene scene = new Scene(griglia2 , 290, 700);
 			scene.getStylesheets().add("it/edu/iisgubbio/gioco/style.css");
 			
-			stradaW.setId("strada");
-			
+//			posizione punteggio migliore e id css
 			lBest.setLayoutX(15);
 			lBest.setLayoutY(40);
 			lBest.setId("best");
-			
+//			posizione punti e id css
 			punti.setLayoutY(20);
 			punti.setLayoutX(15);
 			punti.setId("punti");
+//			y e id css della strada
+			stradaW.setY(y);
+			stradaW.setId("strada");
+//			x, y e grandezza di Bugs Bunny
+			bugsW.setX(290);
+			bugsW.setY(700);
+			bugsW.setFitHeight(100);
+			bugsW.setFitWidth(100);
 //			posizione X
 			posX[0]=16;
 			posX[1]=61;
@@ -207,19 +221,19 @@ public class cassataCarRacing extends Application{
 			posX[3]=149;
 			posX[4]=193;
 			posX[5]=239;
-			
+//			posizione game over
 			gameW.setX(700);
 			gameW.setY(700);
 			
 			griglia2.getChildren().add(stradaW);
-			
+//			creazione macchine del traffico
 			crea(carW1,griglia2);
 			crea(carW2,griglia2);
 			crea(carW3,griglia2);
 			crea(carW4,griglia2);
 			crea(carW5,griglia2);
 			crea(carW6,griglia2);
-			
+//			posizione x delle macchine del traffico
 			for(int i=0;i<carW1.length;i++) {
 				carW1[i].setX(posX[0]);
 				carW2[i].setX(posX[1]);
@@ -228,7 +242,7 @@ public class cassataCarRacing extends Application{
 				carW5[i].setX(posX[4]);
 				carW6[i].setX(posX[5]);
 			}
-			
+//			generazione posizione y delle macchine del traffico
 			genera(carW1);
 			genera2(carW2);
 			genera(carW3);
@@ -243,12 +257,7 @@ public class cassataCarRacing extends Application{
 			
 			griglia2.setPadding(new Insets(10));
 			griglia2.getChildren().add(bugsW);
-			stradaW.setY(y);
-			bugsW.setX(290);
-			bugsW.setY(700);
-			bugsW.setFitHeight(100);
-			bugsW.setFitWidth(100);
-			
+//			aggiunge al Pane il giocatore e imposta la posizione x, y
 			if(blu) {
 				griglia2.getChildren().add(mBluW);
 				mBluW.setX(x);
@@ -292,7 +301,7 @@ public class cassataCarRacing extends Application{
 			}
 			
 			scene.setOnKeyPressed(e->muovi(e));
-			
+//			evento alla chiusura della finestra
 			addEventHandler(WindowEvent.WINDOW_CLOSE_REQUEST, e->{
 				griglia2.getChildren().remove(mBluW);
 				blu=false;
@@ -312,22 +321,25 @@ public class cassataCarRacing extends Application{
 				tempo.stop();
 				tempo2.stop();
 			});
-			
+//			dimensione game over
 			gameW.setFitHeight(210);
 			gameW.setFitWidth(200);
+//			x, y e id css scritta riavvia
 			riavvia.setLayoutX(700);
 			riavvia.setLayoutY(700);
 			riavvia.setId("riavvia");
+			
 			griglia2.getChildren().add(gameW);
 			griglia2.getChildren().add(punti);
 			griglia2.getChildren().add(lBest);
 			griglia2.getChildren().add(riavvia);
+			
 			getIcons().add(new Image("it/edu/iisgubbio/gioco/icona.png"));
 		    setTitle("CCR: Cassata Car Racing");
 		    setScene(scene);
-		    setResizable(true);
+		    setResizable(false);
 		  }
-
+//		timeline del punteggio
 		private void aggiorna2() {
 			if(vivo) {
 				punteggio+=1;
@@ -339,19 +351,17 @@ public class cassataCarRacing extends Application{
 				lBest.setText("Best Score: " + best);
 			}
 		}
-
+		
 		private void muovi(KeyEvent e) {
 //			  rotazione macchina
-				int yGame = (int) gameW.getY();
-				int xGame = (int) gameW.getX();
-				if(e.getCode() == KeyCode.LEFT && yGame == 700 && xGame==700) {
+				if(e.getCode() == KeyCode.LEFT && vivo) {
 					rotazione-=5;
 				}
 				
-				if(e.getCode() == KeyCode.RIGHT && yGame == 700 && xGame==700) {
+				if(e.getCode() == KeyCode.RIGHT && vivo) {
 					rotazione+=5;
 				}
-				
+//				fa ripartire il gioco
 				if(!vivo) {
 					if(e.getCode() == KeyCode.SPACE) {
 						bugsW.setX(290);
@@ -384,11 +394,11 @@ public class cassataCarRacing extends Application{
 					rotazione+=5;
 				}
 				
-				if(e.getCode() != KeyCode.RIGHT && e.getCode() != KeyCode.LEFT && yGame == 700 && xGame==700) {
+				if(e.getCode() != KeyCode.RIGHT && e.getCode() != KeyCode.LEFT && vivo) {
 					bugsW.setX(95);
 					bugsW.setY(100);
 				}
-				
+//				imposta rotazione al giocatore
 				if(blu) {
 					mBluW.setRotate(rotazione);
 				}
@@ -411,7 +421,7 @@ public class cassataCarRacing extends Application{
 					mBluW.setRotate(rotazione);
 				}
 			}
-		  
+//		timeline principale del gioco
 		public void aggiorna() {
 			
 //			movimento strada
@@ -475,7 +485,7 @@ public class cassataCarRacing extends Application{
 				mBluW.setRotate(rotazione);
 				mBluW.setX(x);
 			}
-			
+//			movimento del traffico
 			for(int i=0;i<carW1.length;i++) {
 				y1=(int) carW1[i].getY()+velocita;
 				y2=(int) carW2[i].getY()+velocita;
@@ -491,7 +501,7 @@ public class cassataCarRacing extends Application{
 				carW5[i].setY(y5);
 				carW6[i].setY(y6);
 			}
-			
+//			genera y del traffico quando esce dallo schermo
 			if(y1>=700) {
 				genera(carW1);
 			}
@@ -510,7 +520,7 @@ public class cassataCarRacing extends Application{
 			if(y6>=700) {
 				genera2(carW6);
 			}
-			
+//			collisioni traffico con giocatore
 			if(gialla) {
 				collisioni(carW1,carW2,carW3,carW4,carW5,carW6,mGiallaW);
 			}
@@ -568,32 +578,33 @@ public class cassataCarRacing extends Application{
 			if(velocita == 6) {lDifficolta.setText("Difficolta: FACILE");}
 			if(velocita == 8) {lDifficolta.setText("Difficolta: MEDIO");}
 			if(velocita == 10) {lDifficolta.setText("Difficolta: DIFFICILE");}
-			
+//			label difficoltà impostata x, y e id css
 			lDifficolta.setLayoutX(290);
 			lDifficolta.setLayoutY(70);
 			lDifficolta.setId("lDiff");
-			
+//			pulstanti selezione della difficoltà
+//			id
 			bFacile.setId("facile");
 			bMedio.setId("medio");
 			bDifficile.setId("difficile");
-			
+//			x
 			bFacile.setLayoutX(335);
 			bMedio.setLayoutX(335);
 			bDifficile.setLayoutX(335);
-			
+//			y
 			bFacile.setLayoutY(150);
 			bMedio.setLayoutY(200);
 			bDifficile.setLayoutY(250);
-			
+//			azione al click
 			bFacile.setOnAction(e->falcie());
 			bMedio.setOnAction(e->medio());
 			bDifficile.setOnAction(e->difficile());
-			
+//			pulsante salva ed esci azione, x, y e id
 			bEsci.setOnAction(e->esci());
 			bEsci.setLayoutX(300);
 			bEsci.setLayoutY(340);
 			bEsci.setId("esci");
-			
+//			git e immagini dimenzione e posizione
 			giammaW.setX(0);
 			giammaW.setY(152);
 			giammaW.setFitHeight(140);
@@ -624,28 +635,28 @@ public class cassataCarRacing extends Application{
 		private void esci() {
 			hide();
 		}
-
+//		azione pulsante difficile
 		private void difficile() {
 			velocita = 10;
 			lDifficolta.setText("Difficolta: DIFFICILE");
 			velS = 13;
 		}
-
+//		azione pulsante medio
 		private void medio() {
 			velocita = 8;
 			lDifficolta.setText("Difficolta: MEDIO");
 			velS = 11;
 		}
-
+//		azione pulsante facile
 		private void falcie() {
 			velocita = 6;
 			lDifficolta.setText("Difficolta: FACILE");
 			velS = 11;
 		}
-		
 		}
 	
 	public class FinestraVeicolo extends Stage{
+//		radio button per selezionare il veicolo
 		ToggleGroup veicoli = new ToggleGroup();
 		RadioButton macchinaBlu = new RadioButton();
 		RadioButton macchinaRossa = new RadioButton();
@@ -653,6 +664,7 @@ public class cassataCarRacing extends Application{
 		RadioButton vespaRb = new RadioButton();
 		RadioButton moto2 = new RadioButton();
 		RadioButton moto3 = new RadioButton();
+//		nomi dei veicoli
 		Label macc1 = new Label("Macchina Blu");
 		Label macc2 = new Label("Macchina Rossa");
 		Label macc3 = new Label("Macchina Gialla");
@@ -671,14 +683,14 @@ public class cassataCarRacing extends Application{
 			bVespa=false;
 			bMoto1=false;
 			bMoto2=false;
-			
+//			classe css nomi veicoli
 			macc1.getStyleClass().add("lNomi");
 			macc2.getStyleClass().add("lNomi");
 			macc3.getStyleClass().add("lNomi");
 			lMoto1.getStyleClass().add("lNomi");
 			lMoto2.getStyleClass().add("lNomi");
 			lMoto3.getStyleClass().add("lNomi");
-			
+//			rotazione macchine e moto
 			mBluW.setRotate(0);
 			mRossaW.setRotate(0);
 			mGiallaW.setRotate(0);
@@ -686,7 +698,7 @@ public class cassataCarRacing extends Application{
 			moto1W.setRotate(0);
 			moto2W.setRotate(0);
 			
-	        Button btn = new Button();
+	        Button esci = new Button("Salva ed Esci");
 	        
 			griglia2.setId("paginaVeicoli");
 			griglia2.setPadding(new Insets(10));
@@ -702,14 +714,14 @@ public class cassataCarRacing extends Application{
 			griglia2.getChildren().add(vespaW);
 			griglia2.getChildren().add(moto1W);
 			griglia2.getChildren().add(moto2W);
-			griglia2.getChildren().add(btn);
+			griglia2.getChildren().add(esci);
 			griglia2.getChildren().add(macc1);
 			griglia2.getChildren().add(macc2);
 			griglia2.getChildren().add(macc3);
 			griglia2.getChildren().add(lMoto1);
 			griglia2.getChildren().add(lMoto2);
 			griglia2.getChildren().add(lMoto3);
-			
+//			posizione nomi macchine e moto
 			macc1.setLayoutX(145);
 			macc1.setLayoutY(135);
 			
@@ -727,56 +739,57 @@ public class cassataCarRacing extends Application{
 			
 			lMoto2.setLayoutX(650);
 			lMoto2.setLayoutY(355);
-			
+//			posizione macchine e moto
 			mBluW.setX(195);
 			mRossaW.setX(445);
 			mGiallaW.setX(695);
+			
 			mBluW.setY(50);
 			mRossaW.setY(50);
 			mGiallaW.setY(50);
+			
 			vespaW.setX(195);
 			moto1W.setX(445);
 			moto2W.setX(695);
+			
 			vespaW.setY(270);
 			moto1W.setY(270);
 			moto2W.setY(270);
-			
-//			macchina 1
+//			macchina blu posizione, toggle, id
 			macchinaBlu.setToggleGroup(veicoli);
 			macchinaBlu.setLayoutX(200);
 			macchinaBlu.setLayoutY(160);
-//			macchina 2
+			macchinaBlu.setId("m1");
+//			macchina rossa posizione, toggle, id
 			macchinaRossa.setToggleGroup(veicoli);
 			macchinaRossa.setLayoutX(450);
 			macchinaRossa.setLayoutY(160);
-//			macchina 3
+			macchinaRossa.setId("m2");
+//			macchina gialla posizione, toggle, id
 			macchinaGialla.setToggleGroup(veicoli);
 			macchinaGialla.setLayoutX(700);
 			macchinaGialla.setLayoutY(160);
-			
-			macchinaBlu.setId("m1");
-			macchinaRossa.setId("m2");
 			macchinaGialla.setId("m3");
-			
-			vespaRb.setId("m1");
-			moto2.setId("m2");
-			moto3.setId("m3");
-			
+//			vespa posizione, toggle, id
 			vespaRb.setToggleGroup(veicoli);
 			vespaRb.setLayoutX(200);
 			vespaRb.setLayoutY(380);
-			
+			vespaRb.setId("m1");
+//			moto 1 gialla posizione, toggle, id
 			moto2.setToggleGroup(veicoli);
 			moto2.setLayoutX(450);
 			moto2.setLayoutY(380);
-			
+			moto2.setId("m2");
+//			moto 2 gialla posizione, toggle, id
 			moto3.setToggleGroup(veicoli);
 			moto3.setLayoutX(698);
 			moto3.setLayoutY(380);
-			
-	        btn.setText("Salva ed Esci");
-	        btn.setId("save");
-	        btn.setOnAction((ActionEvent event) -> {
+			moto3.setId("m3");
+//			posizione e azione pulsante salva ed esci
+			esci.setLayoutX(330);
+			esci.setLayoutY(440);
+	        esci.setId("save");
+	        esci.setOnAction((ActionEvent event) -> {
 	        	if(macchinaBlu.isSelected()) {
 	        		blu=true;
 	        	}
@@ -798,9 +811,6 @@ public class cassataCarRacing extends Application{
 	        	
 	            hide();
 	        });
-	        
-			btn.setLayoutX(330);
-			btn.setLayoutY(440);
 			
 			getIcons().add(new Image("it/edu/iisgubbio/gioco/icona.png"));
 		    setTitle("CCR: Cassata Car Racing");
@@ -808,7 +818,7 @@ public class cassataCarRacing extends Application{
 		    setResizable(true);
 		  }
 		}
-	
+//	funzione per le collisioni tra giocatore e traffico
 	public void collisioni(ImageView traffico1[], ImageView traffico2[], ImageView traffico3[], ImageView traffico4[], ImageView traffico5[], ImageView traffico6[], ImageView macchina) {
 		for(int i=0;i<traffico1.length;i++) {
 			if(traffico1[i].getBoundsInParent().intersects(macchina.getBoundsInParent())) {
@@ -855,7 +865,7 @@ public class cassataCarRacing extends Application{
 			}
 		}
 	}
-	
+//	funzione per generare il traffico 1
 	public void genera(ImageView traffico[]) {
 		for(int i=0;i<traffico.length;i++) {
 			int r = random(100, 1000);
@@ -875,7 +885,7 @@ public class cassataCarRacing extends Application{
 			}
 		}
 	}
-	
+//	funzione per generare il traffico 2
 	public void genera2(ImageView traffico[]) {
 		for(int i=0;i<traffico.length;i++) {
 			int r = random(3100, 4000);
@@ -895,7 +905,7 @@ public class cassataCarRacing extends Application{
 			}
 		}
 	}
-	
+//	funzione per creare le macchine del traffico
 	public void crea(ImageView traffico[],Pane pannello) {
 		for(int i=0;i<traffico.length;i++) {
 			traffico[i] = new ImageView(macchina);
@@ -905,7 +915,7 @@ public class cassataCarRacing extends Application{
 			traffico[i].getStyleClass().add("traffico");
 		}
 	}
-	
+//	funzione che genera numeri random
 	public int random(int min, int max) {
 	    Random random = new Random();
 	    return random.nextInt(max - min) + min;
